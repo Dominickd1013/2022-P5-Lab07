1d4c08a24995d360d14fd840c33ddf90c64d62a1
Dominick <domenico.damato@vtsd.com>
2022-11-30 12:29:18 -0500

    11/30

---
+++ Scenery.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.*;
+public class Scenery {
+
+    public class scenery extends Applet{
+
+    public void paint(Graphics g){
+            g.drawLine(1, 1, 1, 100);
+        }
+    }
+}
